f3e6a1cf00999d29f5731692a1a34683a98e3614
mac-n <39732428+mac-n@users.noreply.github.com>
2021-04-12 03:21:11 +0100

    can edit only second column

--- basictabledisplay.R
+++ basictabledisplay.R
@@ -4,7 +4,9 @@ shinyApp(
   ui = fluidPage(DTOutput('tbl')),
   server = function(input, output) {
     output$tbl = renderDT(
-      exportcosts, options = list(lengthChange = FALSE),editable=TRUE
+      exportcosts, options = list(lengthChange = FALSE),editable=list(target = "cell", disable = list(columns = c(0:0)))
     )
   }
 )
+
+
