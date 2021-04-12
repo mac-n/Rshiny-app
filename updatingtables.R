8da132fb63fd066e0ad8a30fdbfff310cd70d61e
mac-n <39732428+mac-n@users.noreply.github.com>
2021-04-12 06:47:29 +0100

    outputs cfs output as list

---
+++ updatingtables.R
@@ -0,0 +1,30 @@
+library(shiny)
+library(DT)
+library(FSelector)
+shinyApp(
+  ui = fluidPage(
+    sidebarLayout(
+      sidebarPanel(
+         numericInput("lamda", label = "lamda - cost weighting", value = 0.01),
+         actionButton("go", "Go"),
+         htmlOutput("x2")
+      ),
+      mainPanel(
+    DTOutput('tbl')))),
+  server = function(input, output) {
+    output$tbl = renderDT(
+      exportcosts, options = list( pageLength = 10, info = FALSE, lengthMenu = list(c(15, -1), c("10", "All"))),editable=list(target = "cell", disable = list(columns = c(0:0)))
+    )
+    
+    output$x2<-eventReactive(input$go, {
+      list1<-cfs(CDRSB~.,joinedcosts)
+      paste(list1,collapse="<br>")
+      }
+
+     
+    )
+  
+  }
+)
+
+
