e5a4a4c24224677671bf108bf8c6d09d23a12e1f
mac-n <39732428+mac-n@users.noreply.github.com>
2021-04-12 03:52:22 +0100

    has numeric input and sidebar layout

--- sidebarLayout.R
+++ sidebarLayout.R
@@ -3,14 +3,19 @@ library(DT)
 shinyApp(
   ui = fluidPage(
     sidebarLayout(
-      sidebarPanel(),
+      sidebarPanel(
+         numericInput("lamda", label = "lamda - cost weighting", value = 0.01) 
+      ),
       mainPanel(
     DTOutput('tbl')))),
   server = function(input, output) {
     output$tbl = renderDT(
       exportcosts, options = list( pageLength = 10, info = FALSE, lengthMenu = list(c(15, -1), c("10", "All"))),editable=list(target = "cell", disable = list(columns = c(0:0)))
     )
-    
+    observeEvent(input$do, {
+      session$sendCustomMessage(type = 'testmessage',
+                                message = 'Thank you for clicking')
+    })
   
   }
 )
