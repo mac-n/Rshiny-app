5df1bd52f562f51f1a9fb4c24e2a060e7d6d9090
mac-n <39732428+mac-n@users.noreply.github.com>
2021-04-12 03:32:43 +0100

    sidebar layout working

--- sidebarLayout.R
+++ sidebarLayout.R
@@ -1,11 +1,17 @@
 library(shiny)
 library(DT)
 shinyApp(
-  ui = fluidPage(DTOutput('tbl')),
+  ui = fluidPage(
+    sidebarLayout(
+      sidebarPanel(),
+      mainPanel(
+    DTOutput('tbl')))),
   server = function(input, output) {
     output$tbl = renderDT(
       exportcosts, options = list( pageLength = 10, info = FALSE, lengthMenu = list(c(15, -1), c("10", "All"))),editable=list(target = "cell", disable = list(columns = c(0:0)))
     )
+    
+  
   }
 )
 
